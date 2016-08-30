3eadc2cfc8359a677be4eb6948579881a9c46e9c
Vincent <panouilleres.vincent@yahoo.fr>
2016-08-30 11:22:51 +0200

    Servlet et jsp pour consultation de compte

--- ProxiBanquev2/src/com/clementvincent2software/proxibanquesi/presentation/ConsultCompteServlet.java
+++ ProxiBanquev2/src/com/clementvincent2software/proxibanquesi/presentation/ConsultCompteServlet.java
@@ -1,11 +1,17 @@
 package com.clementvincent2software.proxibanquesi.presentation;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.clementvincent2software.proxibanquesi.domaine.Client;
+import com.clementvincent2software.proxibanquesi.service.ClientService;
 
 /**
  * Servlet implementation class ConsultCompteServlet
@@ -36,6 +42,19 @@ public class ConsultCompteServlet extends HttpServlet {
 	}
 
 	protected void traitement(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Etape 1 : Récupération des paramètres de la requète
+		int idClient = Integer.parseInt(request.getParameter("idClient"));
+		
+		// Etape 2 : Soumettre les paramètres de la requete à la couche service
+		ClientService clientService = new ClientService();
+		Client client =  clientService.lireClient(idClient);
+		
+		// Etape 3 : Réponse à l'utilisateur
+		RequestDispatcher dispatcher;
+		HttpSession maSession = request.getSession();
+		maSession.setAttribute("client", client);
+		dispatcher = request.getRequestDispatcher("compteclient.jsp");		
+		dispatcher.forward(request, response);
 		
 	}
 }
